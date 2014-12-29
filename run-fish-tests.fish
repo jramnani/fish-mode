eaf0b4203a2c59d0121ac5a9d8407d2007290800
Jeff Ramnani <jeff@jefframnani.com>
2014-12-29 13:35:21 -0600

    ert-runner means less is needed in shell wrapper.

--- run-fish-tests.fish
+++ run-fish-tests.fish
@@ -1,17 +1,7 @@
 #!/usr/bin/env fish
 
-set -l OUTPUT /tmp/fish-shell-tests-output.txt
-
 # Install dependencies via Cask
 cask install
 
 # Run the tests
 cask exec ert-runner -L .
-
-if test $status = 0
-    echo "Success -- All tests passed"
-    rm $OUTPUT
-else
-    cat $OUTPUT
-    echo "FAIL - one or more tests failed"
-end
